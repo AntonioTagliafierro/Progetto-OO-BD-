658ed4ab90239755b069cd297eacf4c1e74616be
Antonio Tagliafierro <78149278+AntonioTagliafierro@users.noreply.github.com>
2021-02-13 23:12:47 +0100

    Update DBConnection.java
    Fix problema di connessione

--- Progetto[OODB2021] gruppo 15 traccia 2/src/dbConfig/DBConnection.java
+++ Progetto[OODB2021] gruppo 15 traccia 2/src/dbConfig/DBConnection.java
@@ -1,62 +1,45 @@
 package dbConfig;
 
-import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 
 public class DBConnection {
 
-	private static DBConnection instance;
-	private Connection connection = null;
-	private String USERNAME;
-	private String PASSWORD;
-	private String IP;
-	private String PORT;
-	private String DB;
-	private String URL = "jbdc:postegresql://" + IP + ":" + PORT + "/" + DB;
+    private static DBConnection instance;
+    private Connection connection;
+   
+    private final String username = "postgres";
+    private final String password = "password";
+    private final String port = "5432";
+    private final String IP = "localhost";
+    private final String nomeDB = "ProjectsManagementDB";
+    private final String url = "jdbc:postgresql://"+IP+":"+port+"/"+nomeDB;
 
-	// metodo per caricare le proprietà del Database da file
-	public void loadProperties() throws Exception {
-		Properties proprietà = new Properties();
-		try {
-			FileInputStream config = new FileInputStream("db.properties");
-			proprietà.load(config);
-		} catch (Exception e) {
-			System.out.println("Errore dati configurazione");
-		}
-		USERNAME = proprietà.getProperty("User");
-		PASSWORD = proprietà.getProperty("Password");
-		IP = proprietà.getProperty("IP");
-		PORT = proprietà.getProperty("Port");
-		DB = proprietà.getProperty("DBname");
-	}
+    private DBConnection() throws SQLException {
+        try {
+            Class.forName("org.postgresql.Driver");
+            this.connection = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Database Connection Creation Failed : " + ex.getMessage());
+        }
+    }
 
-	private DBConnection() throws SQLException {
+    public Connection getConnection() {
+        return connection;
+    }
 
-		try {
-			Class.forName("org.postgresql.Driver");
-			connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    public static DBConnection getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new DBConnection();
+        } else if (instance.getConnection().isClosed()) {
+            instance = new DBConnection();
+        }
 
-		} catch (ClassNotFoundException ex) {
-			System.out.println("Database Connection Creation Failed : " + ex.getMessage());
-		}
-
-	}
-
-	public Connection getConnection() {
-		return connection;
-	}
-
-	public static DBConnection getInstance() throws SQLException {
-		if (instance == null) {
-			instance = new DBConnection();
-		} else if (instance.getConnection().isClosed()) {
-			instance = new DBConnection();
-		}
-
-		return instance;
-	}
-
+        return instance;
+    }
 }
