6be3e4ab6bfb670457787cf02eda0d7f2b845f40
Gianpietro <75445473+gianpietropanico@users.noreply.github.com>
2021-02-20 17:16:07 +0100

    Add files via upload

---
+++ ProgettoDao.java
@@ -0,0 +1,12 @@
+package daos;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import entity.Progetto;
+
+public interface ProgettoDao {
+	
+	
+	public ArrayList<Progetto> RecuperaGeneralit‡Progetto() throws SQLException;
+
+}
