41965275740831629fe169e1b3539c4a71898d66
Gianpietro <75445473+gianpietropanico@users.noreply.github.com>
2021-03-01 15:39:20 +0100

    Add files via upload

--- Progetto[OODB2021] gruppo 15 traccia 2/src/dao_impl/ProgettoDaoImpl.java
+++ Progetto[OODB2021] gruppo 15 traccia 2/src/dao_impl/ProgettoDaoImpl.java
@@ -15,7 +15,10 @@ public class ProgettoDaoImpl implements ProgettoDao {
 
 	//Prepared Statement
 	private PreparedStatement recuperaGeneralit‡PS;
-	
+	private PreparedStatement recuperaStatusPS;
+	private PreparedStatement recuperaStatuseTipologiaPS;
+	private PreparedStatement recuperaTipologiaPS;
+	private PreparedStatement eliminaProgettoPS;
 	
 	public ProgettoDaoImpl(Connection connection) {
 
@@ -29,7 +32,38 @@ public class ProgettoDaoImpl implements ProgettoDao {
 
 		e.printStackTrace();
 	}
-
+	
+	try {
+		recuperaStatusPS = connessione.prepareStatement(
+				"SELECT NOMEPROGETTO, CODP, DATAI, TIPOLOGIA,STATUS FROM PROGETTO WHERE STATUS LIKE ?");
+	} catch (SQLException e) {
+		
+		e.printStackTrace();
+	}
+	
+	try {
+		recuperaStatuseTipologiaPS = connessione.prepareStatement(
+				"SELECT NOMEPROGETTO, CODP, DATAI, TIPOLOGIA,STATUS FROM PROGETTO WHERE STATUS LIKE ? AND TIPOLOGIA LIKE ?");
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+	try {
+		recuperaTipologiaPS = connessione.prepareStatement(
+				"SELECT NOMEPROGETTO, CODP, DATAI, TIPOLOGIA,STATUS FROM PROGETTO WHERE TIPOLOGIA LIKE ? ");
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+	try {
+		eliminaProgettoPS = connessione.prepareStatement("DELETE FROM PROGETTO WHERE CODP = ? ");
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
 	}
 	
 	public ArrayList<Progetto> RecuperaGeneralit‡Progetto() throws SQLException {
@@ -53,6 +87,92 @@ public class ProgettoDaoImpl implements ProgettoDao {
 		return progetti; 
 	}
 
+	@Override
+	public ArrayList<Progetto> RecuperaProgettiStatus(String status) throws SQLException {
+		
+		recuperaStatusPS.setString(1, status);
+		ResultSet rs = recuperaStatusPS.executeQuery();
+		ArrayList<Progetto> progetti = new ArrayList<Progetto>();
+
+		while (rs.next()) {
+			Progetto progetto = new Progetto();
+			progetto.setCodProgetto(rs.getString("CODP"));
+			progetto.setNomeProgetto(rs.getString("NOMEPROGETTO"));
+			progetto.setDataInizioProgetto(rs.getDate("DATAI"));
+			progetto.setStatusProgetto(rs.getString("STATUS"));
+			progetto.setTipologiaProgetto(rs.getString("TIPOLOGIA"));
+			
+
+			progetti.add(progetto);
+		}
+		rs.close();
+
+		return progetti; 
+	}
+
+	@Override
+	public ArrayList<Progetto> RecuperaProgettiStatuseTipologia(String status, String tipologia) throws SQLException {
+		
+		recuperaStatuseTipologiaPS.setString(1, status);
+		recuperaStatuseTipologiaPS.setString(2, tipologia.toUpperCase());
+		
+		ResultSet rs = recuperaStatuseTipologiaPS.executeQuery();
+		ArrayList<Progetto> progetti = new ArrayList<Progetto>();
+
+		while (rs.next()) {
+			Progetto progetto = new Progetto();
+			progetto.setCodProgetto(rs.getString("CODP"));
+			progetto.setNomeProgetto(rs.getString("NOMEPROGETTO"));
+			progetto.setDataInizioProgetto(rs.getDate("DATAI"));
+			progetto.setStatusProgetto(rs.getString("STATUS"));
+			progetto.setTipologiaProgetto(rs.getString("TIPOLOGIA"));
+			
+
+			progetti.add(progetto);
+		}
+		rs.close();
+
+		return progetti; 
+	
+	}
+
+	@Override
+	public ArrayList<Progetto> RecuperaProgettiTipologia(String tipologia)	throws SQLException {
+		
+		recuperaTipologiaPS.setString(1, tipologia.toUpperCase());
+		ResultSet rs = recuperaTipologiaPS.executeQuery();
+		ArrayList<Progetto> progetti = new ArrayList<Progetto>();
+		
+		while (rs.next()) {
+			Progetto progetto = new Progetto();
+			progetto.setCodProgetto(rs.getString("CODP"));
+			progetto.setNomeProgetto(rs.getString("NOMEPROGETTO"));
+			progetto.setDataInizioProgetto(rs.getDate("DATAI"));
+			progetto.setStatusProgetto(rs.getString("STATUS"));
+			progetto.setTipologiaProgetto(rs.getString("TIPOLOGIA"));
+			
+
+			progetti.add(progetto);
+		}
+		rs.close();
+
+		return progetti; 
+	}
+
+	@Override
+	public void eliminaProgetto(String codProgetto) throws SQLException {
+		eliminaProgettoPS.setString(1, codProgetto);
+		eliminaProgettoPS.executeUpdate();
+		
+		
+	}
+	
+
+	}
+		
+		
+
+
 	
 	
-}
+
